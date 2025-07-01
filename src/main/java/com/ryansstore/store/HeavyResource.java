a954d3925b3a760d577a474d9fe8446f7df30cd5
Ryan Griffin <ryanjoegriffin@gmail.com>
2025-07-01 14:28:54 -0700

    formatting + clean-up

--- src/main/java/com/ryansstore/store/HeavyResource.java
+++ src/main/java/com/ryansstore/store/HeavyResource.java
@@ -3,7 +3,7 @@ package com.ryansstore.store;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
-// Used as an example for Lazy Initialization
+// Used as an example for Lazy Initialization. Won't include in AppConfig.java
 @Component
 @Lazy
 public class HeavyResource {
