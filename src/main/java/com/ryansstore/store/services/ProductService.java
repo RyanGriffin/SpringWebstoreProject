83e00cd69bb1dd54d66c4b0b078107c40f314b7c
Ryan Griffin <ryanjoegriffin@gmail.com>
2025-07-11 22:49:23 -0700

    Added methods to ProductService.java

--- src/main/java/com/ryansstore/store/services/ProductService.java
+++ src/main/java/com/ryansstore/store/services/ProductService.java
@@ -1,19 +1,35 @@
 package com.ryansstore.store.services;
 
+import com.ryansstore.store.entities.Product;
+import com.ryansstore.store.repositories.CategoryRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 import com.ryansstore.store.repositories.ProductRepository;
 import lombok.AllArgsConstructor;
-
 import java.math.BigDecimal;
 
 @AllArgsConstructor
 @Service
 public class ProductService {
     private final ProductRepository productRepository;
+    private final CategoryRepository categoryRepository;
 
     @Transactional
     public void deleteProductById(Long id) { productRepository.deleteById(id); }
+
+    @Transactional
+    public void addProductWithCategory(String name, BigDecimal price, String description, Byte categoryId) {
+        var product = Product.builder()
+                .name(name)
+                .price(price)
+                .description(description)
+                .build();
+
+        var category = categoryRepository.findById(categoryId).orElseThrow(() -> new RuntimeException("Category not found"));
+        product.setCategory(category);
+
+        productRepository.save(product);
+    }
 
     // demonstration of custom queries with @Query annotation
     @Transactional
