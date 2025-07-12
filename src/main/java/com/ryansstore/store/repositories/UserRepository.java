1fce96124f7ed2eea6d3320056c0e69b4362eea9
Ryan Griffin <ryanjoegriffin@gmail.com>
2025-07-11 22:41:21 -0700

    Added derived query methods to UserRepository.java

--- src/main/java/com/ryansstore/store/repositories/UserRepository.java
+++ src/main/java/com/ryansstore/store/repositories/UserRepository.java
@@ -1,7 +1,30 @@
 package com.ryansstore.store.repositories;
 
 import com.ryansstore.store.entities.User;
+import com.ryansstore.store.entities.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 public interface UserRepository extends CrudRepository<User, Long> {
+    // Strings!
+    List<User> findByName(String name); // SELECT * FROM users WHERE name = ?
+    List<User> findByNameLike(String name); // SELECT * FROM users WHERE name LIKE ?
+    List<User> findByNameNotLike(String name); // SELECT * FROM users WHERE name NOT LIKE ?
+    List<User> findByNameContaining(String name);
+    List<User> findByNameStartingWith(String name);
+    List<User> findByNameEndingWith(String name);
+    List<User> findByNameEndingWithIgnoreCase(String name);
+
+    // Multiple Conditions
+    List<User> findByNameStartingWithAndEmailEndingWithIgnoreCaseOrderById(String name, String email);
+
+    // Sort (ORDER BY)
+    List<User> findByNameOrderById(String name);
+    List<User> findByNameOrderByIdDesc(String name);
+
+    // Limit
+    List<User> findTop5ByName(String name);
+    List<User> findFirst5ByNameLikeOrderByEmail(String name);
 }
