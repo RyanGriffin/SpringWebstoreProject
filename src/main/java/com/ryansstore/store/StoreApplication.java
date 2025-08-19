f7462baa69c66dda3fe55ba8fc8b67e04f44b0ba
Ryan Griffin <ryanjoegriffin@gmail.com>
2025-08-18 18:19:39 -0700

    removed unused variable

--- src/main/java/com/ryansstore/store/StoreApplication.java
+++ src/main/java/com/ryansstore/store/StoreApplication.java
@@ -7,6 +7,6 @@ import org.springframework.context.ApplicationContext;
 @SpringBootApplication
 public class StoreApplication {
     public static void main(String[] args) {
-        ApplicationContext context = SpringApplication.run(StoreApplication.class, args);
+        SpringApplication.run(StoreApplication.class, args);
     }
 }
