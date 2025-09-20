634f812fe39cac855ff9333fc90ea18de373658f
Ryan Griffin <ryanjoegriffin@gmail.com>
2025-09-20 16:51:02 -0700

    enhance CartDto's toDto() method

--- src/main/java/com/ryansstore/store/mappers/CartMapper.java
+++ src/main/java/com/ryansstore/store/mappers/CartMapper.java
@@ -1,14 +1,15 @@
 package com.ryansstore.store.mappers;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import com.ryansstore.store.entities.Cart;
 import com.ryansstore.store.dtos.CartDto;
 import com.ryansstore.store.entities.CartItem;
 import com.ryansstore.store.dtos.CartItemDto;
-import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface CartMapper {
+    @Mapping(target = "totalPrice", expression = "java(cart.getTotalPrice())")
     CartDto toDto(Cart cart);
 
     @Mapping(target = "totalPrice", expression = "java(cartItem.getTotalPrice())")
