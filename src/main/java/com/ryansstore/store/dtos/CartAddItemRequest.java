6def49735660508f5ad01209a4d34637281c1696
Ryan Griffin <ryanjoegriffin@gmail.com>
2025-09-19 23:20:35 -0700

    added @NotNull validation

--- src/main/java/com/ryansstore/store/dtos/CartAddItemRequest.java
+++ src/main/java/com/ryansstore/store/dtos/CartAddItemRequest.java
@@ -1,8 +1,10 @@
 package com.ryansstore.store.dtos;
 
 import lombok.Data;
+import jakarta.validation.constraints.NotNull;
 
 @Data
 public class CartAddItemRequest {
+    @NotNull
     private Long productId;
 }
