b65bcbc5a6ae6f7430cfbe3498b6a7c2b5b80648
Ryan Griffin <ryanjoegriffin@gmail.com>
2025-09-19 18:20:45 -0700

    updated validation error messages

--- src/main/java/com/ryansstore/store/dtos/UserRegisterRequest.java
+++ src/main/java/com/ryansstore/store/dtos/UserRegisterRequest.java
@@ -8,18 +8,18 @@ import jakarta.validation.constraints.Email;
 @Data
 public class UserRegisterRequest {
     @NotBlank(message = "name is required!") // ensures string isn't empty or whitespace
-    @Size(max = 255, message = "name must be less than 255 characters")
+    @Size(max = 255, message = "name must be less than 255 characters!")
     private String name;
 
-    @NotBlank(message = "email is required")
-    @Email(message = "email must be valid")
+    @NotBlank(message = "email is required!")
+    @Email(message = "email must be valid!")
     private String email;
 
-    @NotBlank(message = "phone number is required")
-    @Size(min = 13, max = 13, message = "must be a full phone number - i.e. '(555)555-5555'")
+    @NotBlank(message = "phone number is required!")
+    @Size(min = 13, max = 13, message = "must be a full phone number! - i.e. '(555)555-5555'")
     private String phoneNumber;
 
-    @NotBlank(message = "password is required")
-    @Size(min = 6, max = 25, message = "password must be between 6 and 25 characters")
+    @NotBlank(message = "password is required!")
+    @Size(min = 6, max = 25, message = "password must be between 6 and 25 characters!")
     private String password;
 }
