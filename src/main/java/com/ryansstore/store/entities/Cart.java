055ba170665ea6a2536bcfd26f5e04d0c875ef12
Ryan Griffin <ryanjoegriffin@gmail.com>
2025-09-19 21:42:36 -0700

    added UUID generation strategy for id field

--- src/main/java/com/ryansstore/store/entities/Cart.java
+++ src/main/java/com/ryansstore/store/entities/Cart.java
@@ -1,9 +1,6 @@
 package com.ryansstore.store.entities;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.*;
 import java.util.Date;
 
@@ -17,6 +14,7 @@ import java.util.Date;
 @Table(name = "carts")
 public class Cart {
     @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     @Column(name = "id")
     private String id;
 
