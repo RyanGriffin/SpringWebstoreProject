cf665b9688665dbfb4ff3a257a54aa762d392b30
Ryan Griffin <ryanjoegriffin@gmail.com>
2025-09-19 23:36:41 -0700

    removed CartItemRepository.java

--- src/main/java/com/ryansstore/store/repositories/CartItemRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ryansstore.store.repositories;
-
-import com.ryansstore.store.entities.CartItem;
-import org.springframework.data.repository.CrudRepository;
-import java.util.UUID;
-
-public interface CartItemRepository extends CrudRepository<CartItem, UUID> {
-}
