f84ed82e98c6ec9caed4c45e698d00f93f0d77b7
Ryan Griffin <ryanjoegriffin@gmail.com>
2025-09-19 22:16:24 -0700

    created CartController.java

---
+++ src/main/java/com/ryansstore/store/controllers/CartController.java
@@ -0,0 +1,26 @@
+package com.ryansstore.store.controllers;
+
+import com.ryansstore.store.entities.Cart;
+import com.ryansstore.store.repositories.CartRepository;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+import org.springframework.http.ResponseEntity;
+import lombok.AllArgsConstructor;
+import java.net.URI;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/carts")
+public class CartController {
+    private final CartRepository cartRepository;
+
+    @PostMapping
+    public ResponseEntity<Cart> createCart(@RequestBody Cart cart, UriComponentsBuilder uriBuilder) {
+        Cart newCart = new Cart();
+        cartRepository.save(newCart);
+
+        URI uri =  uriBuilder.path("/carts/{id}").buildAndExpand(newCart.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(newCart);
+    }
+}
