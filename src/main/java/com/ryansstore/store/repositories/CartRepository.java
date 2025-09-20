f3ec5488b1b644edeb3645c57f2bed11ea5390e1
Ryan Griffin <ryanjoegriffin@gmail.com>
2025-09-20 16:49:18 -0700

    added getCartWithItems() for eager loading of cartItems

--- src/main/java/com/ryansstore/store/repositories/CartRepository.java
+++ src/main/java/com/ryansstore/store/repositories/CartRepository.java
@@ -2,7 +2,14 @@ package com.ryansstore.store.repositories;
 
 import com.ryansstore.store.entities.Cart;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.Query;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface CartRepository extends CrudRepository<Cart, UUID> {
+    @EntityGraph(attributePaths = "items.product")
+    @Query("SELECT c FROM Cart c WHERE c.id = :id")
+    Optional<Cart> getCartWithItems(@Param("id") UUID cartId);
 }
