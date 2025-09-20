3faccaeb631a05fdfecf9b2784b227f8ee43cf50
Ryan Griffin <ryanjoegriffin@gmail.com>
2025-09-19 20:09:56 -0700

    added unused query method for posterity

--- src/main/java/com/ryansstore/store/repositories/ProfileRepository.java
+++ src/main/java/com/ryansstore/store/repositories/ProfileRepository.java
@@ -1,12 +1,19 @@
 package com.ryansstore.store.repositories;
 
 import com.ryansstore.store.entities.Profile;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.Query;
+
 import java.util.List;
 
 public interface ProfileRepository extends CrudRepository<Profile, Long> {
-    // Below method name is long/unreadable, can be simplified with @Query (found in UserRepository)
+    // Below method name is long/unreadable, can be simplified with @Query below
     @EntityGraph(attributePaths = "user")
     List<Profile> findByLoyaltyPointsGreaterThanOrderByUserEmail(int points);
+
+    // This fetches users, so should be in UserRepository.java, but leaving it here for later reference
+    @Query("SELECT u.id AS id, u.email AS email FROM User u JOIN Profile p ON u.id = p.id WHERE p.loyaltyPoints > :points ORDER BY u.email")
+    List<Profile> findQualifiedUsers(@Param("points") int points);
 }
