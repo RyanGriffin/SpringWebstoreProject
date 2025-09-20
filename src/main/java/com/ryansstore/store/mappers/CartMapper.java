0ccff8ac08f36c95c02f3f1d2ec8aebd9ae6e947
Ryan Griffin <ryanjoegriffin@gmail.com>
2025-09-20 00:15:47 -0700

    added mapping for totalPrice

--- src/main/java/com/ryansstore/store/mappers/CartMapper.java
+++ src/main/java/com/ryansstore/store/mappers/CartMapper.java
@@ -5,10 +5,12 @@ import com.ryansstore.store.entities.Cart;
 import com.ryansstore.store.dtos.CartDto;
 import com.ryansstore.store.entities.CartItem;
 import com.ryansstore.store.dtos.CartItemDto;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface CartMapper {
     CartDto toDto(Cart cart);
 
+    @Mapping(target = "totalPrice", expression = "java(cartItem.getTotalPrice())")
     CartItemDto toDto(CartItem cartItem);
 }
