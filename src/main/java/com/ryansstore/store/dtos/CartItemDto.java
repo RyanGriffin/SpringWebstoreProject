7f467a1e97c9721a3507524078a1816949b3f58e
Ryan Griffin <ryanjoegriffin@gmail.com>
2025-09-20 00:06:36 -0700

    changed product field from type Product to CartProductDto

--- src/main/java/com/ryansstore/store/dtos/CartItemDto.java
+++ src/main/java/com/ryansstore/store/dtos/CartItemDto.java
@@ -1,4 +1,11 @@
 package com.ryansstore.store.dtos;
 
+import lombok.Data;
+import java.math.BigDecimal;
+
+@Data
 public class CartItemDto {
+    private CartProductDto product;
+    private int quantity;
+    private BigDecimal totalPrice;
 }
