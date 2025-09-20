5290aa4b13b60fabebe789d82f19bb1f38d1e23e
Ryan Griffin <ryanjoegriffin@gmail.com>
2025-09-19 23:53:33 -0700

    deleted CartItemMapper.java and moved logic to CartMapper.java

--- src/main/java/com/ryansstore/store/mappers/CartMapper.java
+++ src/main/java/com/ryansstore/store/mappers/CartMapper.java
@@ -1,5 +1,7 @@
 package com.ryansstore.store.mappers;
 
+import com.ryansstore.store.dtos.CartItemDto;
+import com.ryansstore.store.entities.CartItem;
 import org.mapstruct.Mapper;
 import com.ryansstore.store.entities.Cart;
 import com.ryansstore.store.dtos.CartDto;
@@ -7,4 +9,6 @@ import com.ryansstore.store.dtos.CartDto;
 @Mapper(componentModel = "spring")
 public interface CartMapper {
     CartDto toDto(Cart cart);
+
+    CartItemDto toDto(CartItem cartItem);
 }
