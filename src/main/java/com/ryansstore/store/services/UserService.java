c2abd541e05cf0aa2b28210339876ebb1f9507a2
Ryan Griffin <ryanjoegriffin@gmail.com>
2025-10-08 16:33:33 -0700

    created UserService class to implement UserDetailsService for DaoAuthenticationProvider

---
+++ src/main/java/com/ryansstore/store/services/UserService.java
@@ -0,0 +1,27 @@
+package com.ryansstore.store.services;
+
+import org.springframework.stereotype.Service;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import com.ryansstore.store.repositories.UserRepository;
+import lombok.AllArgsConstructor;
+import java.util.Collections;
+
+@AllArgsConstructor
+@Service
+public class UserService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        var user = userRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException(email));
+
+        return new User(
+                user.getEmail(),
+                user.getPassword(),
+                Collections.emptyList()
+            );
+    }
+}
