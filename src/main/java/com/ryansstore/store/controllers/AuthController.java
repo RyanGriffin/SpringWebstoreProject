af10e5cc5bdaf72b837529e3cc7853131b3d9fc4
Ryan Griffin <ryanjoegriffin@gmail.com>
2025-10-08 16:50:28 -0700

    fixed logic bug in response

--- src/main/java/com/ryansstore/store/controllers/AuthController.java
+++ src/main/java/com/ryansstore/store/controllers/AuthController.java
@@ -1,9 +1,7 @@
 package com.ryansstore.store.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -29,7 +27,7 @@ public class AuthController {
                 )
         );
 
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        return ResponseEntity.ok().build();
     }
 }
 
