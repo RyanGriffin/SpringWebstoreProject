ecb61f15bbd9a2c63894c7c7ba389f951c5011dd
Ryan Griffin <ryanjoegriffin@gmail.com>
2025-10-07 22:56:26 -0700

    added password encoding functionality

--- src/main/java/com/ryansstore/store/config/SecurityConfig.java
+++ src/main/java/com/ryansstore/store/config/SecurityConfig.java
@@ -7,11 +7,18 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
     @Bean
     public SecurityFilterChain springSecureFilterChain(HttpSecurity http) throws Exception {
         http
