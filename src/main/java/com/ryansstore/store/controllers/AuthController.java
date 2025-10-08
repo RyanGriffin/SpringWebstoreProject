ba9d142f21739ca7379e61414b4b0f567f2837e1
Ryan Griffin <ryanjoegriffin@gmail.com>
2025-10-08 16:40:54 -0700

    updated login() to use Spring Security's AuthenticationManager interface

--- src/main/java/com/ryansstore/store/controllers/AuthController.java
+++ src/main/java/com/ryansstore/store/controllers/AuthController.java
@@ -1,28 +1,33 @@
 package com.ryansstore.store.controllers;
 
-import com.ryansstore.store.services.AuthService;
-import com.ryansstore.store.dtos.LoginRequest;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
-import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import com.ryansstore.store.dtos.LoginRequest;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @AllArgsConstructor
 @Tag(name = "Auth")
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
-    private final AuthService authService;
+    private final AuthenticationManager authenticationManager;
 
     @PostMapping("/login")
     public ResponseEntity<Void> login(@Valid @RequestBody LoginRequest loginRequest) {
-        if(authService.loginSuccessful(loginRequest.getEmail(), loginRequest.getPassword()))
-            return ResponseEntity.ok().build();
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginRequest.getEmail(),
+                        loginRequest.getPassword()
+                )
+        );
 
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
     }
