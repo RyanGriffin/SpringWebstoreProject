83ddd397b76dd55112fedbf2148d11316b0cacdd
Ryan Griffin <ryanjoegriffin@gmail.com>
2025-10-07 23:43:19 -0700

    added @Email validation for email field

--- src/main/java/com/ryansstore/store/dtos/LoginRequest.java
+++ src/main/java/com/ryansstore/store/dtos/LoginRequest.java
@@ -1,11 +1,13 @@
 package com.ryansstore.store.dtos;
 
+import jakarta.validation.constraints.Email;
 import lombok.Data;
 import jakarta.validation.constraints.NotBlank;
 
 @Data
 public class LoginRequest {
     @NotBlank(message = "email is required!")
+    @Email
     private String email;
 
     @NotBlank(message = "password is required!")
