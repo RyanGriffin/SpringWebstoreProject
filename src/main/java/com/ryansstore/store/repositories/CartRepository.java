b662e22d2fca69e98c6833a046432397eff09037
Ryan Griffin <ryanjoegriffin@gmail.com>
2025-10-13 13:45:43 -0700

    formatting

--- src/main/java/com/ryansstore/store/repositories/CartRepository.java
+++ src/main/java/com/ryansstore/store/repositories/CartRepository.java
@@ -1,13 +1,15 @@
 package com.ryansstore.store.repositories;
 
-import com.ryansstore.store.entities.Cart;
+import org.springframework.stereotype.Repository;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import com.ryansstore.store.entities.Cart;
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface CartRepository extends CrudRepository<Cart, UUID> {
     @EntityGraph(attributePaths = "items.product")
     @Query("SELECT c FROM Cart c WHERE c.id = :id")
