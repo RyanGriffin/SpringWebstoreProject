a587b05b7210cb37ac8753d2b83337cb5d068b3f
Ryan Griffin <ryanjoegriffin@gmail.com>
2025-10-13 12:18:36 -0700

    created CheckoutRequest and CheckoutResponse DTOs

---
+++ src/main/java/com/ryansstore/store/dtos/CheckoutRequest.java
@@ -0,0 +1,11 @@
+package com.ryansstore.store.dtos;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import java.util.UUID;
+
+@Data
+public class CheckoutRequest {
+    @NotNull(message = "cart id is required!")
+    private UUID cartId;
+}
---
+++ src/main/java/com/ryansstore/store/dtos/CheckoutResponse.java
@@ -0,0 +1,8 @@
+package com.ryansstore.store.dtos;
+
+import lombok.Data;
+
+@Data
+public class CheckoutResponse {
+    private Long orderId;
+}
