1a950dfe8e93aa043ca76c6cd3aa8a6e8aeb69be
Ryan Griffin <ryanjoegriffin@gmail.com>
2025-10-20 19:43:57 -0700

    improved error handling

--- src/main/java/com/ryansstore/store/controllers/CheckoutController.java
+++ src/main/java/com/ryansstore/store/controllers/CheckoutController.java
@@ -1,18 +1,16 @@
 package com.ryansstore.store.controllers;
 
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import com.ryansstore.store.exceptions.CartNotFoundException;
 import com.ryansstore.store.exceptions.EmptyCartException;
-import com.ryansstore.store.repositories.CartRepository;
-import com.ryansstore.store.services.CartService;
 import com.ryansstore.store.services.CheckoutService;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.http.ResponseEntity;
+import com.ryansstore.store.dtos.ErrorDto;
 import com.ryansstore.store.dtos.CheckoutRequest;
 import com.ryansstore.store.dtos.CheckoutResponse;
 import lombok.AllArgsConstructor;
 import jakarta.validation.Valid;
-import java.util.Map;
 import java.util.UUID;
 
 @AllArgsConstructor
@@ -20,13 +18,9 @@ import java.util.UUID;
 @RequestMapping("/checkout")
 public class CheckoutController {
     private final CheckoutService checkoutService;
-    private final CartService cartService;
-    private final CartRepository cartRepository;
 
-    // 2. create a new order using
     @PostMapping
     public ResponseEntity<CheckoutResponse> checkout(@Valid @RequestBody CheckoutRequest request) {
-        // 1. get cart with cartId in request
         UUID cartId = request.getCartId();
 
         CheckoutResponse response = checkoutService.checkout(cartId);
@@ -34,12 +28,12 @@ public class CheckoutController {
     }
 
     @ExceptionHandler(CartNotFoundException.class)
-    public ResponseEntity<Map<String,String>> handleCartNotFound() {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Map.of("error", "cart not found!"));
+    public ResponseEntity<ErrorDto> handleCartNotFound() {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorDto("cart not found!"));
     }
 
     @ExceptionHandler(EmptyCartException.class)
-    public ResponseEntity<Map<String,String>> handleEmptyCartFound() {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Map.of("error", "cart is empty!"));
+    public ResponseEntity<ErrorDto> handleEmptyCartFound() {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorDto("cart is empty!"));
     }
 }
--- src/main/java/com/ryansstore/store/controllers/GlobalExceptionHandler.java
+++ src/main/java/com/ryansstore/store/controllers/GlobalExceptionHandler.java
@@ -3,7 +3,9 @@ package com.ryansstore.store.controllers;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import com.ryansstore.store.dtos.ErrorDto;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -18,5 +20,10 @@ public class GlobalExceptionHandler {
         });
 
         return ResponseEntity.badRequest().body(errors);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorDto> handleUnreadableMessage() {
+        return ResponseEntity.badRequest().body(new ErrorDto("Invalid request body!"));
     }
 }
