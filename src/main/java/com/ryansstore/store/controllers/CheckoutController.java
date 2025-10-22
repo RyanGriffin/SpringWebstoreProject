99d26643588daea6d7d4f553d93c0d4b090f357f
Ryan Griffin <ryanjoegriffin@gmail.com>
2025-10-22 16:15:12 -0700

    added basic webhook logic for checkouts

--- src/main/java/com/ryansstore/store/controllers/CheckoutController.java
+++ src/main/java/com/ryansstore/store/controllers/CheckoutController.java
@@ -1,6 +1,11 @@
 package com.ryansstore.store.controllers;
 
 import com.ryansstore.store.dtos.CheckoutResponse;
+import com.stripe.exception.SignatureVerificationException;
+import com.stripe.model.Event;
+import com.stripe.model.StripeObject;
+import com.stripe.net.Webhook;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,18 +15,53 @@ import com.ryansstore.store.dtos.CheckoutRequest;
 import com.ryansstore.store.exceptions.CartNotFoundException;
 import com.ryansstore.store.exceptions.EmptyCartException;
 import com.ryansstore.store.exceptions.PaymentException;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import jakarta.validation.Valid;
 
-@AllArgsConstructor
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/checkout")
 public class CheckoutController {
     private final CheckoutService checkoutService;
 
+    @Value("${stripe.webhookSecretKey}")
+    private String webhookSecretKey;
+
     @PostMapping
     public CheckoutResponse checkout(@Valid @RequestBody CheckoutRequest request) {
             return checkoutService.checkout(request);
+    }
+
+    @PostMapping("/webhook")
+    public ResponseEntity<Void> handleWebhook(
+                @RequestHeader("Stripe-Signature") String signature,
+                @RequestBody String payload
+    ) {
+        try {
+            Event event =  Webhook.constructEvent(payload, signature, webhookSecretKey);
+            System.out.println(event.getType());
+
+            // if -> charge event: cast stripeObject to Charge object
+            // if -> payment_intent.succeeded: cast stripeObject to PaymentIntent object
+            // note: when handling this event, we should check eventId hasn't been handled before
+            // note: we should store this info somewhere (db, cache, etc.), but will deal with that later
+            // note: may consider this out-of-scope for this project...
+            StripeObject stripeObject = event.getDataObjectDeserializer().getObject().orElse(null);
+
+            switch(event.getType()) {
+                case "payment_intent.succeeded" -> {
+                    // update order status to PAID
+                }
+                case "payment_intent.failed" -> {
+                    // update order status to FAILED
+                }
+            }
+
+            return ResponseEntity.ok().build();
+
+        } catch (SignatureVerificationException e) {
+            return ResponseEntity.badRequest().build();
+        }
     }
 
     @ExceptionHandler(CartNotFoundException.class)
