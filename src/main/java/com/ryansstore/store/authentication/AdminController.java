bfa87296d14edbef951f82c0313d832bd38a53c7
Ryan Griffin <ryanjoegriffin@gmail.com>
2025-11-09 19:21:19 -0800

    added swagger.io support

--- src/main/java/com/ryansstore/store/authentication/AdminController.java
+++ src/main/java/com/ryansstore/store/authentication/AdminController.java
@@ -3,7 +3,9 @@ package com.ryansstore.store.authentication;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
+@Tag(name = "Admin")
 @RestController
 @RequestMapping("/admin")
 public class AdminController {
@@ -12,3 +14,5 @@ public class AdminController {
         return "Hello Admin!";
     }
 }
+
+
