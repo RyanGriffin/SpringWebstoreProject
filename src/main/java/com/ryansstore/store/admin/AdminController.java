02e520150071d8635b510dcf38a52e7f39defec7
Ryan Griffin <ryanjoegriffin@gmail.com>
2025-11-17 17:33:52 -0800

    refactor: created 'admin' package

--- src/main/java/com/ryansstore/store/admin/AdminController.java
+++ src/main/java/com/ryansstore/store/admin/AdminController.java
@@ -1,4 +1,4 @@
-package com.ryansstore.store.authentication;
+package com.ryansstore.store.admin;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
