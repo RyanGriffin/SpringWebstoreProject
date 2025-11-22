c8efec2498da3083ee5e4d1657c59d53780551e3
Ryan Griffin <ryanjoegriffin@gmail.com>
2025-11-21 16:51:34 -0800

    update: enhance API documentation with swagger

--- src/main/java/com/ryansstore/store/admin/AdminController.java
+++ src/main/java/com/ryansstore/store/admin/AdminController.java
@@ -4,11 +4,13 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.Operation;
 
 @Tag(name = "Admin")
 @RestController
 @RequestMapping("/admin")
 public class AdminController {
+    @Operation(summary = "Verifies that the current user has admin privileges.")
     @GetMapping("/hello")
     public String sayHello() {
         return "Hello Admin!";
