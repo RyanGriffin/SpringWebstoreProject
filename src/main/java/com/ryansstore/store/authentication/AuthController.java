b6e8a8ecf966eb4a2520b176fa70e6102e713ff3
Ryan Griffin <ryanjoegriffin@gmail.com>
2025-11-21 17:02:55 -0800

    update: enhance API documentation with swagger

--- src/main/java/com/ryansstore/store/authentication/AuthController.java
+++ src/main/java/com/ryansstore/store/authentication/AuthController.java
@@ -7,12 +7,14 @@ import org.springframework.security.authentication.BadCredentialsException;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.Cookie;
 import jakarta.validation.Valid;
-import com.ryansstore.store.users.UserMapper;
 import com.ryansstore.store.users.User;
 import com.ryansstore.store.users.UserDto;
 import com.ryansstore.store.users.UserNotFoundException;
-import lombok.AllArgsConstructor;
+import com.ryansstore.store.users.UserMapper;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import lombok.AllArgsConstructor;
 
 @Tag(name = "Auth")
 @AllArgsConstructor
@@ -23,8 +25,13 @@ public class AuthController {
     private final JwtConfig jwtConfig;
     private final UserMapper userMapper;
 
+    @Operation(summary = "Logs in a user.")
     @PostMapping("/login")
-    public JwtResponse login(@Valid @RequestBody LoginRequest loginRequest, HttpServletResponse response) {
+    public JwtResponse login(
+            @Parameter(description = "Request DTO containing an email and password.")
+            @Valid @RequestBody LoginRequest loginRequest,
+            @Parameter(description = "HTTP response containing the refresh token cookie.")
+            HttpServletResponse response) {
         LoginResponse loginResponse = authService.login(loginRequest);
 
         Jwt accessToken = loginResponse.getAccessToken();
@@ -40,11 +47,15 @@ public class AuthController {
         return new JwtResponse(accessToken.toString());
     }
 
+    @Operation(summary = "Generates a new access token using the refresh token.")
     @PostMapping("/refresh")
-    public JwtResponse refresh(@CookieValue(value = "refreshToken") String refreshToken) {
+    public JwtResponse refresh(
+            @Parameter(description = "Refresh token used for generating new access token")
+            @CookieValue(value = "refreshToken") String refreshToken) {
         return new JwtResponse(authService.refreshToken(refreshToken).toString());
     }
 
+    @Operation(summary = "Returns a response containing the details of the current user.")
     @GetMapping("/me")
     public ResponseEntity<UserDto> me() {
         User user =  authService.getCurrentUser();
