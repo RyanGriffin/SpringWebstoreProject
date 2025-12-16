deb8c1d6c23339891f9501426525f2313b127237
Ryan Griffin <ryanjoegriffin@gmail.com>
2025-12-15 21:46:26 -0800

    change: added filter chain output for debugging

--- src/main/java/com/ryansstore/store/StoreApplication.java
+++ src/main/java/com/ryansstore/store/StoreApplication.java
@@ -1,11 +1,22 @@
 package com.ryansstore.store;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class StoreApplication {
     public static void main(String[] args) {
         SpringApplication.run(StoreApplication.class, args);
+    }
+
+    @Bean
+    public ApplicationRunner filterDebug(org.springframework.context.ApplicationContext ctx) {
+        return args -> {
+            System.out.println("=== Filters ===");
+            ctx.getBeansOfType(jakarta.servlet.Filter.class)
+                    .forEach((name, f) -> System.out.println(name + " -> " + f.getClass().getName()));
+        };
     }
 }
